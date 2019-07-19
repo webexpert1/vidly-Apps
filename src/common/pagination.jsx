4eb42dbbc40c840f62d42f09bc99e02a820f6ae7
Richard <richokoye1@gmail.com>
2019-07-19 21:53:22 +0100

    add pagination

---
+++ src/common/pagination.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import _ from 'lodash';
+
+const Pagination = props => {
+        const { itemsCount, pageSize, currentPage } = props;
+
+        const pagesCount = Math.ceil(itemsCount/pageSize);
+
+        if(pagesCount === 1) return null;
+
+        const pages = _.range(1, pagesCount + 1);
+        console.log(currentPage);
+
+        return (
+            <div>
+               <nav aria-label="Page navigation example">
+                    <ul className="pagination">
+                        {pages.map(page => (
+                            <li key={page} className={ page === currentPage ? 'page-item active': 'page-item' }><a onClick={() => props.onPageChange(page)} className="page-link" href="#">{page}</a></li> 
+                        ))}
+                                              
+                    </ul>
+                    </nav> 
+            </div>
+        )
+}
+
+export default Pagination;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Like from '../common/like';
 import { getMovies } from '../services/fakeMovieService';
+import { paginate } from '../utils/paginate';
+import Pagination from '../common/pagination';
+
 
 class Movies extends Component {
     state  = {
-        movies: getMovies()
+        movies: getMovies(),
+        currentPage: 1,
+        pageSize: 4
     }
 
     handelDelete = (movie) => {
@@ -20,8 +25,17 @@ class Movies extends Component {
         this.setState({ movies });
     }
 
+    handlePageChange = (page) => {
+        this.setState({currentPage: page});
+    }
+
+   
     render() {
         const {length: count } = this.state.movies;
+        const {currentPage, pageSize} = this.state;
+        const { movies: allMovies } = this.state;
+
+        const movies = paginate(allMovies, currentPage, pageSize)
 
         if(count === 0) {
             return <p>There are no moviies in the database. </p>
@@ -29,21 +43,23 @@ class Movies extends Component {
         return (
             <div>
                 <p>Showing {this.state.movies.length} movies in the database.</p>
+
+                
                 <table className="table">
                     <thead>
                         <tr>
-                            <td>Title</td>
-                            <td>Genre</td>
-                            <td>Stock</td>
-                            <td>Rate</td>
-                            <td>
+                            <th>Title</th>
+                            <th>Genre</th>
+                            <th>Stock</th>
+                            <th>Rate</th>
+                            <th>
                                
-                            </td>
+                            </th>
                             <td></td>
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.movies.map(movie => (
+                        {movies.map(movie => (
                             <tr key={movie._id}>
                                 <td>{movie.title}</td>
                                 <td>{movie.genre.name}</td>
@@ -58,6 +74,7 @@ class Movies extends Component {
                     </tbody>
 
                 </table>
+                <Pagination itemsCount={count} currentPage={currentPage} pageSize={pageSize} onPageChange={this.handlePageChange} />
             </div>
         )
     }
